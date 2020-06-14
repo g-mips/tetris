33b70aa2fc7921bef70bb2713b5e8864bf726e7e
g-mips <g-mips@users.noreply.github.com>
2020-06-14 16:52:35 -0600

    Add test for positioning the current block

--- test/unit/test_board.c
+++ test/unit/test_board.c
@@ -4,6 +4,20 @@
 
 #include "test_board.h"
 
+START_TEST (test_board_position_current_block_success)
+{
+    int new_y = 5;
+    int new_x = 6;
+
+    current_block_pos.y = 0;
+    current_block_pos.x = 0;
+    board_position_current_block(new_y, new_x);
+
+    ck_assert_int_eq(current_block_pos.y, new_y);
+    ck_assert_int_eq(current_block_pos.x, new_x);
+}
+END_TEST
+
 START_TEST (test_board_set_current_block_success)
 {
     struct block new_block = {
@@ -23,10 +37,14 @@ test_board_suite(void)
     Suite *s = suite_create("Board");
 
     TCase *tc_set_current_block = tcase_create("Set Current Block");
+    TCase *tc_position_current_block = tcase_create("Position Current Block");
 
     tcase_add_test(tc_set_current_block, test_board_set_current_block_success);
+    tcase_add_test(tc_position_current_block,
+        test_board_position_current_block_success);
 
     suite_add_tcase(s, tc_set_current_block);
+    suite_add_tcase(s, tc_position_current_block);
 
     return s;
 }
